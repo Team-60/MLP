8672b80276c670ce1564e075a0acf893b703b8a8
watch24hrs-iiitd <divyansh19464@iiitd.ac.in>
2022-02-25 23:25:15 +0530

    add adaline

---
+++ Madaline/madaline/adaline.py
@@ -0,0 +1,30 @@
+"""
+Adaline algo
+"""
+
+import numpy as np
+
+
+class Perceptron:
+    """
+    Adaline perceptron
+    """
+
+    def __init__(self, dim: int, lr: int = 0.1, bias: bool = True, random_init: bool = True):
+        self.dim = dim
+        self.w = np.random.random(dim + 1) if random_init else np.zeros(dim + 1)
+        self.bias = float(bias)
+        self.lr = lr
+
+    def forward(self, x: np.array) -> float:
+        x_ = np.append(x, self.bias)
+        return (self.w * x_).sum()
+
+    def predict(self, x: np.array) -> int:
+        y_pred = self.forward(x)
+        return 1 if (y_pred >= 0) else -1
+
+    def update_weights(self, x: np.array, d: int):
+        x_ = np.append(x, self.bias)
+        z = self.forward(x)
+        self.w += self.lr * (d - z) * x_
---
+++ Madaline/main.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+############### ADALINE CHECK ###############
+
+from madaline.adaline import Perceptron
+
+# INPUTS
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([0, 0, 0, 1])
+
+y = np.array([1 if it else -1 for it in y])
+
+percep = Perceptron(2, random_init=False)
+
+
+def get_incorrect():
+    for i in range(len(X)):
+        if percep.predict(X[i]) != y[i]:
+            return X[i], y[i]
+    return None
+
+
+iters = 0
+while sample := get_incorrect():
+    sx, sy = sample
+    percep.update_weights(sx, sy)
+    iters += 1
+    if iters > 1000:
+        raise Exception
+
+print("ITERS", iters)
+print("WEIGHTS", percep.w)
+for i in range(X.shape[0]):
+    print()
+    print("Sample", X[i])
+    print("Perceptron output", percep.predict(X[i]))
+    print("Actual output", y[i])
